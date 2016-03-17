00322796321b34754661d4b2a21f028c5e56320d
Christopher Yung <cyung10892@gmail.com>
2016-03-17 10:13:57 -0400

    add method to prune files older than a set time

--- handlers.go
+++ handlers.go
@@ -23,6 +23,68 @@ var c *cache.Cache
 
 func init() {
   c = cache.New(5*time.Minute, 30*time.Second)
+
+  for {
+    err := cleanOld()
+    if err != nil {
+      fmt.Println(err)
+    }
+    time.Sleep(5 * time.Second)
+  }
+}
+
+// remove images older than a set time
+func cleanOld() error {
+  d, err := os.Open("./images")
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  filenames, err := d.Readdirnames(-1)
+
+  for _, filename := range filenames {
+    old, err := isOld(filename)
+    if err != nil {
+      return err
+    }
+
+    if old {
+      err := removeFile(filename)
+      if err != nil {
+        return err
+      }
+    }
+  }
+
+  return nil
+}
+
+// checks if a file is older than a set time
+func isOld(filename string) (bool, error) {
+  const MAX_TIME = 24.0 * 5 // max 5 days (in hours)
+  f, err := os.Open("./images/" + filename)
+  if err != nil {
+    return false, err
+  }
+  defer f.Close()
+
+  info, err := f.Stat()
+  if err != nil {
+    return false, err
+  }
+
+  age := time.Since(info.ModTime())
+  return age.Hours() > MAX_TIME, nil
+}
+
+func removeFile(filename string) error {
+  err := os.Remove("./images/" + filename)
+  if err != nil {
+    return err
+  }
+
+  fmt.Printf("removed %s\n", filename)
+  return nil
 }
 
 func AddImage(w http.ResponseWriter, r *http.Request) {
